a32b80cd41745f5cb27ef02a1cedbb2e5ef656d8
DESKTOP-4BAHKGU\rudrh <bbrotherkk@gmail.com>
2021-09-07 18:23:57 +0900

    git init

---
+++ src/main/java/com/example/glowdayz/GlowdayzApplication.java
@@ -0,0 +1,13 @@
+package com.example.glowdayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowdayzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlowdayzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/glowdayz/GlowdayzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glowdayz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowdayzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
